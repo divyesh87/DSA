21d4efa4650037bec022195b4e93157f439cda62
nerdycryptoboi <divyeshlalwani13@gmail.com>
2022-08-29 18:38:58 +0530

    added LL

--- SortsAndSearch/linkedLists.c
+++ SortsAndSearch/linkedLists.c
@@ -1,37 +1,41 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 
-struct Node{
+struct Node
+{
     int data;
-    struct Node* next;
+    struct Node *next;
 };
 
-int main() {
-    
-    struct Node* first;
-    struct Node* second;
-    struct Node* third;
-    struct Node* ptr;
-    
-    first = malloc(sizeof(struct Node));
-    second = malloc(sizeof(struct Node));
-    third = malloc(sizeof(struct Node));
-    ptr = malloc(sizeof(struct Node));
-    
+int main()
+{
+
+    struct Node *first = malloc(sizeof(struct Node));
+    struct Node *second = malloc(sizeof(struct Node));
+    struct Node *third = malloc(sizeof(struct Node));
+    struct Node *ptr = malloc(sizeof(struct Node));
+
+    // first = malloc(sizeof(struct Node));
+    // second = malloc(sizeof(struct Node));
+    // third = malloc(sizeof(struct Node));
+    // ptr = malloc(sizeof(struct Node));
+
     ptr = first;
-    
+
     first->data = 1;
     first->next = second;
-    
+
     second->data = 2;
     second->next = third;
-    
+
     third->data = 3;
     third->next = NULL;
-    
-    while(1){
+
+    while (1)
+    {
         printf("%d => ", ptr->data);
-        if(ptr->next == NULL){
+        if (ptr->next == NULL)
+        {
             break;
         }
         ptr = ptr->next;
